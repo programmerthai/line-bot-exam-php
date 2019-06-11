6a34124a8e502e2245d9e3e8aa8286b64891dba7
programmerthai <solot_th@hotmail.com>
2019-06-11 22:01:30 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,11 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'EZrlAI4/lZhitJ0wg9Xu3bAFdDLzUkH1pd36sJ4uYwpEAimH9nacWEcNQtmKvIJ09EliE1jlCB70a69ST6+ycAvZEuwWVWJ3Q75wvu+XCuJw4gv8lqjOIqzyx1be5opNj3erz+VDccSKMCgLOcgfygdB04t89/1O/w1cDnyilFU=';
+$access_token = 'EZrlAI4/+ycAvZEuwWVWJ3Q75wvu+XCuJw4gv8lqjOIqzyx1be5opNj3erz+VDccSKMCgLOcgfygdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '4a267af1e5f37eeae660aa807e3f3bfc';
+$channelSecret = '';
 
-$pushID = 'U3a03cacbced3254076884a836fcb0dda';
+$pushID = '';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
